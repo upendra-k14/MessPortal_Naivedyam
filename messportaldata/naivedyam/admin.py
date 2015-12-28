192b7bacfdea0b815bf10a1fa92c8395cf1bef2c
scorpiolegacy <upendra.k14@iiits.in>
2015-12-28 11:08:25 +0530

    Merge done

--- messportaldata/naivedyam/admin.py
+++ messportaldata/naivedyam/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Meal, Mess, Student, Non_Student, Vendor, Account, Booking, Unregister, Menu, Suggestion
 # Register your models here.
+
+admin.site.register(Meal)
+admin.site.register(Mess)
+admin.site.register(Student)
+admin.site.register(Non_Student)
+admin.site.register(Vendor)
+admin.site.register(Booking)
+admin.site.register(Unregister)
+admin.site.register(Menu)
+admin.site.register(Suggestion)
+
--- messportaldata/naivedyam/models.py
+++ messportaldata/naivedyam/models.py
@@ -1,5 +1,6 @@
 # encoding: utf-8
 from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 # Create your models here.
@@ -17,29 +18,46 @@ class Mess(models.Model):
 	vendor = models.CharField(max_length=100)
 
 class Student(models.Model):
-	account_balance = models.FloatField()
+	user = models.OneToOneField(User)
+	account_balance = models.OneToOneField(Account)
 	rollno = models.CharField(max_length=20)
 	default_mess = models.CharField(max_length=100)
 
 class Non_Student(models.Model):
-	account_balance = models.FloatField()
+	user = models.OneToOneField(User)
+	account_balance = models.OneToOneField(Account)
 	default_mess = models.CharField(max_length=100)
 
 class Vendor(models.Model):
-	account_balance = models.FloatField()
+	user = models.OneToOneField(User)
+	account_balance = models.OneToOneField(Account)
 
 class Account(models.Model):
-	balance = models.CharField()
+	balance = models.FloatField()
 	regdate = models.DateTimeField()
 	expdate = models.DateTimeField()
 
-class Booking(models.Model):
-	book_date = models.DateTimeField()
-	
 class Unregister(models.Model):
 	unreg_date = models.DateTimeField()
 
 class Menu(models.Model):
-	mealitems = models.
+	WEEK_DAYS=(
+		('MO','Monday'),
+		('TU','Tuesday'),
+		('WE','Wednesday'),
+		('TH','Thursday'),
+		('FR','Friday'),
+		('SA','Saturday'),
+		('SU','Sunday'),
+	)
+	mealitems = ArrayField(models.CharField(max_length=50))
+	weekday = models.CharField(max_length=20,choices=WEEK_DAYS)
+
+class Suggestion(models.Model):
+	title = models.CharField(max_length=150)
+	iffile = models.BooleanField(default=False)
+	sgg_content = models.CharField(max_length=1000)
+	
+	
 	
 
