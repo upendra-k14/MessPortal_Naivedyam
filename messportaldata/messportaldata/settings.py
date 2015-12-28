8a6397c43da5264f32a61c4c7702a6da29b34f49
scorpiolegacy <upendra.k14@iiits.in>
2015-12-28 14:41:09 +0530

    Models setup done

--- messportaldata/messportaldata/settings.py
+++ messportaldata/messportaldata/settings.py
@@ -104,9 +104,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_MEDIA = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
    os.path.join(BASE_DIR, 'static'),
 )
-   
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'complaint_files')
+MEDIA_URL = '/media/'
+
--- messportaldata/naivedyam/models.py
+++ messportaldata/naivedyam/models.py
@@ -2,14 +2,19 @@
 from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
+from django.utils import timezone
+from datetime import timedelta
 
 # Create your models here.
 
 class Meal(models.Model):
 	name = models.CharField(max_length=30)
-	starttime = models.TimeField()
-	endtime = models.TimeField()
+	booktime = models.DurationField(default=timedelta())
+	canceltime = models.DurationField(default=timedelta())
 	rate = models.FloatField()
+
+	def __str__(self):
+		return self.name
 
 class Mess(models.Model):
 	name = models.CharField(max_length=100)
@@ -17,28 +22,52 @@ class Mess(models.Model):
 	capacity = models.IntegerField()
 	vendor = models.CharField(max_length=100)
 
+	def __str__(self):
+		return self.name
+
+class Account(models.Model):
+        balance = models.FloatField()
+        regdate = models.DateTimeField()
+        expdate = models.DateTimeField()
+
+        def __str__(self):
+                return str(self.balance)
+
+
 class Student(models.Model):
-	user = models.OneToOneField(User)
+	id = models.OneToOneField(User, primary_key=True)
 	account_balance = models.OneToOneField(Account)
 	rollno = models.CharField(max_length=20)
 	default_mess = models.CharField(max_length=100)
 
+	def __str__(self):
+		return self.id.first_name+" "+self.id.last_name
+
 class Non_Student(models.Model):
-	user = models.OneToOneField(User)
+	id = models.OneToOneField(User, primary_key=True)
 	account_balance = models.OneToOneField(Account)
 	default_mess = models.CharField(max_length=100)
+	
+	def __str__(self):
+                return self.id.first_name+" "+self.id.last_name
 
 class Vendor(models.Model):
-	user = models.OneToOneField(User)
+	id = models.OneToOneField(User, primary_key=True)
 	account_balance = models.OneToOneField(Account)
 
-class Account(models.Model):
-	balance = models.FloatField()
-	regdate = models.DateTimeField()
-	expdate = models.DateTimeField()
+	def __str__(self):
+                return self.id.first_name+" "+self.id.last_name
 
 class Unregister(models.Model):
 	unreg_date = models.DateTimeField()
+	mealid = models.ForeignKey(Meal)
+	unreg_date = models.DateField()
+
+class Booking(models.Model):
+	sid = models.ForeignKey(User)
+	mealid = models.ForeignKey(Meal)
+	book_date = models.DateField()
+	mess = models.ForeignKey(Mess)
 
 class Menu(models.Model):
 	WEEK_DAYS=(
@@ -50,14 +79,14 @@ class Menu(models.Model):
 		('SA','Saturday'),
 		('SU','Sunday'),
 	)
+	mealid = models.ForeignKey(Meal)
 	mealitems = ArrayField(models.CharField(max_length=50))
 	weekday = models.CharField(max_length=20,choices=WEEK_DAYS)
 
 class Suggestion(models.Model):
+	user = models.ForeignKey(User)
 	title = models.CharField(max_length=150)
 	iffile = models.BooleanField(default=False)
 	sgg_content = models.CharField(max_length=1000)
-	
-	
-	
+	sgg_file = models.FileField(null=True, blank=True, upload_to='')
 
